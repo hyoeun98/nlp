c3e42f3b72e171cf1a4c89fc41f8c271313b9d77
hyoeun98 <f2921641@naver.com>
2021-10-30 18:47:20 +0900

    watcha crawling

--- Crawling/watcha_crawling.py
+++ Crawling/watcha_crawling.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 import selenium
 import re
 import time
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 
@@ -14,22 +12,31 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 
-def crawl(row):
-    time.sleep(3)
+def crawl(i):
+    time.sleep(1)
     try:
         movie_title = driver.find_element_by_xpath(f'//*[@id="root"]/div[1]/main/div[1]/section/ul/li[{i}]/div[2]/div/div/div[3]/h1')
-    except:
+    except: # 제목이 img
         movie_title = driver.find_element_by_xpath(f'//*[@id="root"]/div[1]/main/div[1]/section/ul/li[{i}]/div[2]/div/div/div[3]/img').get_attribute('alt')
-    else:
+    else: # 제목이 h1
         movie_title = movie_title.text
         
-    movie_info = driver.find_element_by_xpath(f'//*[@id="root"]/div[1]/main/div[1]/section/ul/li[{i}]/div[2]/div/div/div[3]/div[2]/div[2]')
-    movie_rating = driver.find_element_by_xpath(f'//*[@id="root"]/div[1]/main/div[1]/section/ul/li[{i}]/div[2]/div/div/div[3]/div[1]/div[1]/div[2]/div[2]')
-    
-    print(movie_title)
-    print(movie_info.text)
-    print(movie_rating.text)
-    # file print 추가
+    try:
+        moive_type = driver.find_element_by_class_name(f'closed')
+    except: # 영화
+        movie_info = driver.find_element_by_xpath(f'//*[@id="root"]/div[1]/main/div[1]/section/ul/li[{i}]/div[2]/div/div/div[3]/div[2]/div[2]')
+        movie_rating = driver.find_element_by_xpath(f'//*[@id="root"]/div[1]/main/div[1]/section/ul/li[{i}]/div[2]/div/div/div[3]/div[1]/div[1]/div[2]/div[2]')
+    else: # 시즌이 여러개인 드라마
+        movie_info = driver.find_element_by_xpath(f'//*[@id="root"]/div[1]/main/div[1]/section/ul/li[{i}]/div[2]/div/div/div[3]/div[3]/div[2]')
+        movie_rating = driver.find_element_by_xpath(f'//*[@id="root"]/div[1]/main/div[1]/section/ul/li[{i}]/div[2]/div/div/div[3]/div[2]/div[1]/div[2]/div[2]')
+
+#     print(movie_title)
+#     print(movie_info.text)
+#     print(movie_rating.text)
+    data = {'title':movie_title,
+          'story':movie_info.text,
+          'rating':movie_rating.text}
+    return data
 
 
 URL = 'https://watcha.com/sign_in'
@@ -44,30 +51,35 @@ driver.find_element_by_xpath('//*[@id="root"]/div[1]/main/div[1]/main/div/form/d
 driver.find_element_by_xpath('//*[@id="root"]/div[1]/main/div[1]/main/div/form/div[2]/input').send_keys('pw')
 driver.find_element_by_xpath('//*[@id="root"]/div[1]/main/div[1]/main/div/form/div[3]/button').click()
 driver.find_element_by_xpath('//*[@id="root"]/div[1]/main/div[1]/section/ul/li[4]/button/div[2]').click()
+driver.find_element_by_xpath('//*[@id="root"]/div[1]/nav/ul[2]/li[2]/div/div/div/a').click() # 프로필 선택
 
+df = pd.DataFrame(columns=['title', 'story', 'rating'])
 
-driver.find_element_by_xpath('//*[@id="root"]/div[1]/nav/ul[2]/li[2]/div/div/div/a').click()
-
-for i in range(1,100):
+for i in range(1,5):
     movie = driver.find_element_by_xpath(f'//*[@id="root"]/div[1]/main/div[1]/section/ul/li[{i}]/div[1]/ul/li[1]/div/div[1]/div[1]/div[2]')
     ActionChains(driver).move_to_element(movie).pause(1.5).perform()
     hidden_button = driver.find_element_by_class_name('css-g373u1-StyledEmbedButton')
     hidden_button.click()
-    crawl(i)
+    df = df.append(crawl(i),ignore_index=True)
     
     movie = driver.find_element_by_xpath(f'//*[@id="root"]/div[1]/main/div[1]/section/ul/li[{i}]/div[1]/ul/li[2]/div/div[1]/div[1]/div[2]')
     movie.click()
-    crawl(i)
+    df = df.append(crawl(i),ignore_index=True)
     
     movie = driver.find_element_by_xpath(f'//*[@id="root"]/div[1]/main/div[1]/section/ul/li[{i}]/div[1]/ul/li[3]/div/div[1]/div[1]/div[2]')
     movie.click()
-    crawl(i)
+    df = df.append(crawl(i),ignore_index=True)
     
     movie = driver.find_element_by_xpath(f'//*[@id="root"]/div[1]/main/div[1]/section/ul/li[{i}]/div[1]/ul/li[4]/div/div[1]/div[1]/div[2]')
     movie.click()
-    crawl(i)
+    df = df.append(crawl(i),ignore_index=True)
 
     driver.find_element_by_xpath(f'//*[@id="root"]/div[1]/main/div[1]/section/ul/li[{i}]/div[2]/div/div/div[2]').click()
     time.sleep(1)
     driver.find_element_by_tag_name('body').send_keys('Keys.PAGE_DOWN')
+
+
+print(df)
+
+
 
